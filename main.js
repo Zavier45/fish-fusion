8ba815751a93353f91914b1687f794603b9040b8
Ruby Kendrick <kendrick.ruby@gmail.com>
2024-01-18 09:46:37 -0600

    hey y'all look at me go

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const { fishMenu } = require("./restaurant.js");
 //here is where we run the algorithm
 // this is chads comment 
+//this is RUBY
 const chefBudget = 6.99;
 const menu = fishMenu(chefBudget);
 console.log(menu);
